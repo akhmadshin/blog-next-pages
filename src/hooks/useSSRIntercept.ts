83e439599ba0b65d12bcd0c08cac8be8ceb2afc3
akhmadshin <qwasderfasder@gmail.com>
2023-10-29 01:55:52 +0700

    Fix useSSRIntercept

--- src/hooks/useSSRIntercept.ts
+++ src/hooks/useSSRIntercept.ts
@@ -6,23 +6,25 @@ export const useSSRIntercept = () => {
     if (!PageRouter.router?.components) return;
 
     const pageLoader = PageRouter.router.pageLoader;
-    if (!pageLoader) return;
 
-    for (const key in PageRouter.router.components) {
-      if (key !== '/_app') delete PageRouter.router.components[key];
-    }
+    if (!pageLoader) return;
 
     const {loadPage: originalLoadPage} = pageLoader;
     pageLoader.loadPage = (...args) => (
       originalLoadPage
         .apply(pageLoader, args)
-        .then((pageCache) => ({
-          ...pageCache,
-          mod: {
-            ...pageCache.mod,
-            __N_SSP: false,
-          },
-        })))
+        .then((pageCache) => {
+          return {
+            ...pageCache,
+            mod: {
+              ...pageCache.mod,
+              // NOTE: behave as if there is no `getServerSideProps` for the
+              // page so Next won't fetch the result from the server
+              // @see https://github.com/vercel/next.js/blob/13b32ba98179aa94ac2e402f272e5c6a3356d310/packages/next/src/shared/lib/router/router.ts#L2181
+              __N_SSP: false,
+            },
+          }
+        }))
 
     return () => {
       pageLoader.loadPage = originalLoadPage;
